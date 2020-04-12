f6f4592d7bf1a130b8aaa2dcbf22236e3e39a22a
tkgstrator <nasawake.am@gmail.com>
2020-04-12 06:10:08 -0400

    Add Game::Coop headers.

---
+++ include/Game/Coop/Setting.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "types.h"
+
+namespace Game {
+    namespace Coop {
+        class Setting {
+            public:
+                u64 startChangeWaterLevel(int);
+                void reset();
+                void setup(int);
+                /* ... */
+        };
+    };
+};
